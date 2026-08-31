beb4610278389c09b716c0507d54944babbabf08
Toblilj <tobbel93@hotmail.com>
2026-08-31 10:54:52 +0200

    Add callback function for terminal and  added to infome function

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-function infome (whoamI) {
-    if(whoamI=="who am i?"){
+/* Callback functions for terminal--------*/
+function infome (command) {
+    if(command == "who am i?"){
         
         return "Tobias Lilja";
     } 
-    else{
-        return "Ogiltigt kommand"; 
+    else if (command == "help"){
+        const helpArr = ["Who am i?" , "ls" , "uptime" , "contact" , "hire" , "clear" ];
+       return "hjälp text här";
+        
+    } else {
+        return "Ogiltigt kommand";
     }
 
-}
+} 
 
 const input=document.getElementById("terminalInput");
-const output=documet.getElementById("textoutput");
-
-function  getEvent (event) {
-   
-    if(terminalInput ==whoamI){
-        return terminalInput;
-    } else
-        alert("Wrong input");
+const output=document.getElementById("terminalOutput");
 
 
+//Reply-callback function-this checks user input and if valid replies with infome func.
+input.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    const reply = infome(input.value);
 
-}
+    if (reply === "Ogiltigt kommando") {
+      alert("wrong input");
+    } else {
+      output.textContent = reply;
+    }
 
-
-// input.addEventListener("keydown", function (event));
-
-// if (event.key=="Enter"){
-    
-// return whoamI;
-
-// }
+    input.value = "";
+  }
+});
